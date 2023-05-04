b6cb2feff3c0cd379cb9b15477c6f36970ea87c6
Vitalii Dykhal <vitalikbyht1@gmail.com>
2023-05-05 00:05:06 +0300

    fix

--- src/swagger/swagger-paths/index.ts
+++ src/swagger/swagger-paths/index.ts
@@ -2,11 +2,9 @@ import PathPhotographersCurrent from './auth/current.json';
 import PathPhotographersSingIn from './auth/singIn.json';
 
 import PathAlbums from './albums/albums.json';
-// import PathGetAllAlbums from './albums/getAllAlbums.json';
 
 export const paths = {
   ...PathPhotographersSingIn,
   ...PathPhotographersCurrent,
   ...PathAlbums,
-  // ...PathGetAllAlbums,
 };
